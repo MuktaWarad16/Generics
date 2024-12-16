4ad1bc5c81fc7d1087d7eb1d0ab1869b82e4859a
mukta <muktaspersonal@gmail.com>
2024-12-16 16:00:21 +0530

    Biggest of 3 decimal numbers

---
+++ Generics/BiggestOfThreeFloatNumbers.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+
+
+//namespace Practice
+//{
+//    internal class BiggestOfThreeFloatNumbers
+//    {
+//        public static void Main(String[] args)
+//        {
+//            Console.WriteLine("enter 3 numbers");
+
+//            float a = float.Parse(Console.ReadLine());
+//            float b = float.Parse(Console.ReadLine());
+//            float c = float.Parse(Console.ReadLine());
+
+//            if (a > b && a > c)
+//                Console.WriteLine(a + " is biggest");
+//            else if (b > c)
+//                Console.WriteLine(b + " is biggest");
+//            else
+//                Console.WriteLine(c + "  is biggest");
+//        }
+
+
+
+
+//    }
+//}
