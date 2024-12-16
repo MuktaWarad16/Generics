f66768ba0f9cd88ef67691580b6d801d1978b388
mukta <muktaspersonal@gmail.com>
2024-12-16 16:08:39 +0530

    committed

---
+++ Generics/BiggestUsingComparable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    internal class BiggestUsingComparable
+    {
+        public static T FindBiggest<T>(T val1,T val2,T val3) where T:IComparable<T>
+        {
+            if (val1.CompareTo(val2) > 0 && val2.CompareTo(val3) > 0)
+                return val1;
+
+            else if (val2.CompareTo(val3) > 0 && val2.CompareTo(val1) > 0)
+            {
+                return val2;
+            }
+            else
+                return val3;
+        }
+        
+    }
+}
