677c861764ee18c0d60d90adffe02c92fc523b26
mukta <muktaspersonal@gmail.com>
2024-12-16 16:03:52 +0530

    biggest of 3 integers

---
+++ Generics/BiggestOfThreeIntegers.cs
@@ -0,0 +1,50 @@
+﻿//using System;
+//using System.Runtime.InteropServices;
+
+
+//namespace Practice
+//{
+//    internal class BiggestOfThreeIntegers
+//    {
+//        public static void Main(String[] args)
+//        {
+//            Console.WriteLine("enter 3 numbers");
+
+//            Console.WriteLine("enter value of a");
+//            int a = int.Parse(Console.ReadLine());
+
+
+//            Console.WriteLine("enter value of b");
+//            int b = int.Parse(Console.ReadLine());
+
+//            Console.WriteLine("enter value of c");
+//            int c = int.Parse(Console.ReadLine());
+
+//            int big = Biggest(a, b, c);
+//            Console.WriteLine("the biggest number is: " + big);
+
+
+
+
+//            static int Biggest(int a, int b, int c)
+//            {
+
+
+
+//                if (a > b && a > c)
+//                    return a;
+//                else if (b > c)
+//                    return b;
+//                else
+//                    return c;
+
+//            }
+
+
+//        }
+
+
+
+
+//    }
+//}
