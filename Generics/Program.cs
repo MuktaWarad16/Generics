cb908c1bf9738072ece81bcff41c4953cb2e4cd0
mukta <muktaspersonal@gmail.com>
2024-12-16 16:17:57 +0530

    committed

---
+++ Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Generics
+{
+    internal class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine( BiggestUsingComparable.FindBiggest(10,20,30));
+        }
+    }
+}
